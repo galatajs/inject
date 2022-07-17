d0f83e2ab0a90216cff362981b1eb75fca453c07
Sami Salih İbrahimbas <info@ssibrahimbas.com>
2022-07-17 22:48:22 +0300

    inject: quiet updated

--- lib/hooks/inject.hooks.ts
+++ lib/hooks/inject.hooks.ts
@@ -1,14 +1,12 @@
 import { Injector, InjectorCreator } from "../types/inject.types";
 
-export const createInjector: InjectorCreator = <T>(
-  quiet: boolean = false
-): Injector<T> => {
+export const createInjector: InjectorCreator = <T>(): Injector<T> => {
   const map: Map<string, T> = new Map();
   return {
     provide(key: string, value: T): void {
       map.set(key, value);
     },
-    inject(key: string): T | null {
+    inject(key: string, quiet: boolean = false): T | null {
       if (!map.has(key)) {
         if (!quiet) {
           throw new Error(`Injector: key (${key}) not found`);
--- lib/types/inject.types.ts
+++ lib/types/inject.types.ts
@@ -1,6 +1,6 @@
 export interface Injector<T> {
   provide(key: string, value: T): void;
-  inject(key: string): T | null;
+  inject(key: string, quiet?: boolean): T | null;
 }
 
-export type InjectorCreator = <T>(quiet?: boolean) => Injector<T>;
+export type InjectorCreator = <T>() => Injector<T>;
